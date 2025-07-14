9bcd17996f7a969d70dca9f48c246f69fa9f3925
ashutoshv4929 <ashutoshv4929@gmail.com>
2025-07-14 16:29:13 +0530

    Update workbox-config.js

--- src/workbox-config.js
+++ src/workbox-config.js
@@ -1,11 +1,15 @@
 module.exports = {
-	globDirectory: 'public/',
-	globPatterns: [
-		'**/*.{png,json}'
-	],
-	swDest: 'public/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+  globDirectory: '.',
+  globPatterns: [
+    'public/icons/**/*', // हमारे सभी आइकॉन्स
+    'public/manifest.json', // हमारा मैनिफेस्ट
+    '.next/static/chunks/**/*.js', // Next.js के ज़रूरी जावास्क्रिप्ट हिस्से
+    '.next/static/css/**/*.css'   // Next.js की ज़रूरी CSS फाइलें
+  ],
+  swDest: 'public/sw.js',
+  globIgnores: [
+    '**/*.map',
+    'next.config.js',
+    '.next/server/**'
+  ],
 };
